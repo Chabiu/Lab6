1ef0d641852c5296a4294c5a3d19b6db141337a1
kuba <dsw52652@student.dsw.edu.pl>
2024-07-13 01:59:32 +0200

    poprawkaxd

--- python.py
+++ python.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def parse_arguments():
     if len(sys.argv) != 3:
@@ -10,6 +11,22 @@ def parse_arguments():
     
     return input_file, output_file
 
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        print(f"Error reading JSON file: {e}")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
-    input_file, output_file = parse_arguments()
-    print(f"Input file: {input_file}, Output file: {output_file}")
+        input_file, output_file = parse_arguments()
+    if input_file.endswith('.json'):
+        data = load_json(input_file)
+        print(f"Loaded data: {data}")
+    else:
+        print("Input file is not a .json file")
+
